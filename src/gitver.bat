ddb63081234b2ed10bd9512023183cbd6dd89585
Robert Edgar <robert@drive5.com>
2022-01-13 15:29:59 -0800

    Update gitver.bat to create gitver.txt if not found.

--- src/gitver.bat
+++ src/gitver.bat
@@ -1,9 +1,16 @@
 @echo off
 
+if exist gitver.txt (
+	echo gitver.txt found
+) else (
+	echo 000 > gitver.txt
+)
+
 if exist c:\cygwin64\bin\bash.exe (
 	echo bash found
 	c:\cygwin64\bin\bash -c ./gitver.bash
 ) else (
 	echo bash not found
+	echo 000 > gitver.txt
 	exit
 )
