71d661f0b948ba4647c5e78c9e12e3cafa91e6e9
Robert Edgar <robert@drive5.com>
2022-01-13 18:31:20 -0800

    Update gitver.bash to handle error from git.

--- src/gitver.bash
+++ src/gitver.bash
@@ -1,9 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash
 
 PATH=$PATH:/usr/bin
 
 git describe --abbrev=6 --dirty --long --always \
   | sed '-es/^\(.*\)$/"\1"/' \
   > gitver.txt
+
+if [ $? != 0 ] ; then
+	echo unknown > gitver.txt
+fi
 
 cat gitver.txt
