6bcbbd3fc94b9ab886836dfe53fe133076da0f05
Robert Edgar <robert@drive5.com>
2022-02-06 09:50:21 -0800

    Update gitver.bash
    Check if ../.git exists, set git hash to zero if not found.

--- src/gitver.bash
+++ src/gitver.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ! -d ../.git ] ; then
+  if [ ! -f gitver.txt ] then
+    echo "0" > gitver.txt
+  fi
+  echo "Repo not found, git hash set to zero"
+  exit 0
+fi
+
 PATH=$PATH:/usr/bin
 
 git describe --abbrev=6 --dirty --long --always \
