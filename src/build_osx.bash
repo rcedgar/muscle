48b24a4465a35c91a13e4037ab6f371a37617d18
Robert Edgar <robert@drive5.com>
2024-08-21 13:56:06 -0700

    Implement build_osx.bash

--- src/build_osx.bash
+++ src/build_osx.bash
@@ -1,3 +1,3 @@
 rm -f vcxproj_make.py
 wget https://raw.githubusercontent.com/rcedgar/vcxproj_make/806d016/vcxproj_make.py
-./vcxproj_make.py --openmp
+./vcxproj_make.py --openmp --cppcompiler g++-11
