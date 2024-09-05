401db8b2fe207492cebc353397de7a310adac465
Robert Edgar <robert@drive5.com>
2024-09-05 16:54:21 -0700

    test_sw_mm random, ok 9947/10000

--- src/test_sw_mm.cpp
+++ src/test_sw_mm.cpp
@@ -10,6 +10,17 @@ float SWFast_MASM_MegaProf(XDPMem &Mem, const MASM &MA,
 void MakeMegaProfile_AA(const string &Seq,
   vector<vector<byte> > &Prof);
 
+static uint g_MinRandL = 3;
+static uint g_MaxRandL = 7;
+static uint g_MinRandN = 1;
+static uint g_MaxRandN = 5;
+static uint g_RandGapPct = 25;
+
+static uint g_N = 0;
+static uint g_NAllOk = 0;
+static uint g_NPathDiff = 0;
+static uint g_NScoreDiff = 0;
+
 static uint g_LA;
 static uint g_LB;
 static uint g_SeqCountA;
@@ -80,13 +91,9 @@ static MASM *MakeMASM_AAs(const vector<string> &Rows)
 //	  Score, Loi, Loj, Path.c_str());
 //	}
 
-static void Test(const string &sA, const string &B)
+static void Test(const vector<string> &RowsA, const string &B,
+  bool AlwaysLog)
 	{
-	ClearBrute();
-
-	vector<string> RowsA;
-	Split(sA, RowsA, '|');
-
 	g_SeqCountA = SIZE(RowsA);
 	for (uint i = 0; i < g_SeqCountA; ++i)
 		{
@@ -113,16 +120,46 @@ static void Test(const string &sA, const string &B)
 	g_LB = SIZE(B);
 	EnumPathsLocal(g_LA, g_LB, OnPath);
 
-	Log("\n");
-	for (uint i = 0; i < g_SeqCountA; ++i)
-		Log("%s  >A%u\n", RowsA[i].c_str(), i);
-	Log("%s  >B\n", B.c_str());
+	bool AllOk = true;
+	++g_N;
+	if (g_BestPath != MMPath)
+		{
+		Log("@PATHDIFF\n");
+		++g_NPathDiff;
+		AllOk = false;
+		}
+	if (!feq(g_BestScore, MMScore))
+		{
+		Log("@SCOREDIFF\n");
+		++g_NScoreDiff;
+		AllOk = false;
+		}
+	if (AllOk)
+		++g_NAllOk;
 
-	Log("  %7.3g  %s  (%u, %u)  Brute\n",
-	  g_BestScore, g_BestPath.c_str(), g_BestPosA, g_BestPosB);
+	if (!AllOk)
+		{
+		Log("\n");
+		for (uint i = 0; i < g_SeqCountA; ++i)
+			Log("%s  >A%u\n", RowsA[i].c_str(), i);
+		Log("%s  >B\n", B.c_str());
 
-	Log("  %7.3g  %s  (%u, %u)  MASM_Mega \n",
-	  MMScore, MMPath.c_str(), MMLoi, MMLoj);
+		Log("  %7.3g  %s  (%u, %u)  Brute\n",
+		  g_BestScore, g_BestPath.c_str(), g_BestPosA, g_BestPosB);
+
+		Log("  %7.3g  %s  (%u, %u)  MASM_Mega \n",
+		  MMScore, MMPath.c_str(), MMLoi, MMLoj);
+		}
+	}
+
+static void Test(const string &sA, const string &B)
+	{
+	ClearBrute();
+
+	vector<string> RowsA;
+	Split(sA, RowsA, '|');
+
+	Test(RowsA, B, true);
 	}
 
 static void LogPath(const string &sA, const string &B,
@@ -164,14 +201,79 @@ static void LogPath(const string &sA, const string &B,
 	g_PS->m_Trace = false;
 	}
 
+static void GetRandSeq(string &s)
+	{
+	s.clear();
+	uint L = g_MinRandL + randu32()%(g_MaxRandL - g_MinRandL + 1);
+	asserta(L >= g_MinRandL && L <= g_MaxRandL);
+	for (uint i = 0; i < L; ++i)
+		s += g_LetterToCharAmino[randu32()%20];
+	}
+
+static void GetRandRows(vector<string> &Rows)
+	{
+	Rows.clear();
+	uint L = g_MinRandL + randu32()%(g_MaxRandL - g_MinRandL + 1);
+	uint n = g_MinRandN + randu32()%(g_MaxRandN - g_MinRandN + 1);
+	asserta(L >= g_MinRandL && L <= g_MaxRandL);
+	asserta(n >= g_MinRandN && n <= g_MaxRandN);
+
+	for (uint i = 0; i < n; ++i)
+		{
+		string Row;
+		for (uint j = 0; j < L; ++j)
+			{
+			if (randu32()%100 < g_RandGapPct)
+				Row += '-';
+			else
+				Row += g_LetterToCharAmino[randu32()%20];
+			}
+		Rows.push_back(Row);
+		}
+	for (uint ColIdx = 0; ColIdx < L; ++ColIdx)
+		{
+		bool AllGaps = true;
+		for (uint SeqIdx = 0; SeqIdx < n; ++SeqIdx)
+			{
+			if (!isgap(Rows[SeqIdx][ColIdx]))
+				{
+				AllGaps = false;
+				break;
+				}
+			}
+		if (AllGaps)
+			Rows[randu32()%n][ColIdx] = g_LetterToCharAmino[randu32()%20];
+		}
+	}
+
+static void TestRandom()
+	{
+	ClearBrute();
+	vector<string> RowsA;
+	string B;
+	GetRandRows(RowsA);
+	GetRandSeq(B);
+	Test(RowsA, B, false);
+	}
+
 void cmd_test_sw_mm()
 	{
 	PathScorer_MASM_Mega PS;
 	g_PS = &PS;
 
-	Test("SEQ|SEQ", "SEQ");
-	Test("SE-|-EQ", "SEQ");
-	Test("SEV-|V-EQ|DDD-", "WEQ");
+	//Test("SEQ|SEQ", "SEQ");
+	//Test("SE-|-EQ", "SEQ");
+	//Test("SEV-|V-EQ|DDD-", "WEQ");
 	//LogPath("SEV-|V-EQ|DDD-", "WEQ", 1, 1, "MDM");
 	//LogPath("SEV-|V-EQ|DDD-", "WEQ", 2, 1, "MM");
+	const uint ITERS = 10000;
+	for (uint Iter = 0; Iter < ITERS; ++Iter)
+		{
+		ProgressStep(Iter, ITERS, "Running %u ok, %u pathdiff, %u scorediff",
+		  g_NAllOk, g_NPathDiff, g_NScoreDiff);
+		TestRandom();
+		}
+
+	Log("N %u, allok %u, pathdiff %u, scorediff %u\n",
+	  g_N, g_NAllOk, g_NPathDiff, g_NScoreDiff);
 	}
