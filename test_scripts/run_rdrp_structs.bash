d952c3432f61c89a7040da13259c99dca8b6dceb
Robert Edgar <robert@drive5.com>
2024-11-10 07:50:08 -0800

    Implement gunzip for rdrp mega test

--- test_scripts/run_rdrp_structs.bash
+++ test_scripts/run_rdrp_structs.bash
@@ -2,10 +2,15 @@
 
 outdir=../test_output/rdrp
 logdir=../test_logs
-mkdir -p $outdir $logdir
+mkdir -p $outdir $logdir ../tmp
+
+cp ../test_data/rdrp.mega.gz ../tmp
+gunzip ../tmp/rdrp.mega.gz
 
 ../bin/muscle \
-  -super7 ../test_data/rdrp/rdrp.mega \
+  -super7 ../tmp/rdrp/rdrp.mega \
   -guidetreein ../test_data/rdrp/rdrp.newick \
   -output $outdir/rdrp_structs.afa \
   -log ../test_logs/super7_rdrp.log
+
+rm -rf ../tmp
